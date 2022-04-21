6649804c8ce68d78c90d2d06751248dea40076ea
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-04-21 19:04:54 +0600

    first backend data fetch project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = { name, email };
+    fetch("http://localhost:5000/user", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+        console.log("Success:", data);
+      });
+    console.log(user);
+    e.target.name.value = "";
+    e.target.email.value = "";
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the first backend project</h1>
+      <h4>Total Users: {users.length}</h4>
+      <form onSubmit={handleFormSubmit}>
+        <input type="text" name="name" required placeholder="Name" />
+        <br />
+        <input type="email" name="email" required placeholder="Email" />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {" "}
+            id:{user.id} Name: {user.name} Email: {user.gmail}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
